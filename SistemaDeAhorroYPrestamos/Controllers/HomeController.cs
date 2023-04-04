70af8b1e09f87d7e3e3db4b87e7ddd8b86953b3b
chanicua0121 <128559709+chanicua0121@users.noreply.github.com>
2023-04-04 18:52:12 -0400

    ok

--- SistemaDeAhorroYPrestamos/Controllers/HomeController.cs
+++ SistemaDeAhorroYPrestamos/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace SistemaDeAhorroYPrestamos.Controllers
             return View();
         }
 
+        public IActionResult contactenos()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
